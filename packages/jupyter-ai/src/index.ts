d47f56c4052c202da51ab631485da8a72ca33af7
gchow <garson.chow@factset.com>
2024-01-11 12:44:36 -0500

    Reflect theme changes without a refresh
    By leveraging `IThemeManager.themeChanged`, we can listen on theme change signals
    and rebuild the theme object in response. This allows CSS variable changes to reflect
    in the MUI theme without having to refresh the page.

--- packages/jupyter-ai/src/index.ts
+++ packages/jupyter-ai/src/index.ts
@@ -4,7 +4,11 @@ import {
   ILayoutRestorer
 } from '@jupyterlab/application';
 
-import { IWidgetTracker, ReactWidget } from '@jupyterlab/apputils';
+import {
+  IWidgetTracker,
+  ReactWidget,
+  IThemeManager
+} from '@jupyterlab/apputils';
 import { IDocumentWidget } from '@jupyterlab/docregistry';
 import { IGlobalAwareness } from '@jupyter/collaboration';
 import type { Awareness } from 'y-protocols/awareness';
@@ -12,6 +16,7 @@ import { buildChatSidebar } from './widgets/chat-sidebar';
 import { SelectionWatcher } from './selection-watcher';
 import { ChatHandler } from './chat_handler';
 import { buildErrorWidget } from './widgets/chat-error';
+import { setThemeManager } from './theme-provider';
 
 export type DocumentTracker = IWidgetTracker<IDocumentWidget>;
 
@@ -22,8 +27,10 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter_ai:plugin',
   autoStart: true,
   optional: [IGlobalAwareness, ILayoutRestorer],
+  requires: [IThemeManager],
   activate: async (
     app: JupyterFrontEnd,
+    manager: IThemeManager,
     globalAwareness: Awareness | null,
     restorer: ILayoutRestorer | null
   ) => {
@@ -57,6 +64,11 @@ const plugin: JupyterFrontEndPlugin<void> = {
     if (restorer) {
       restorer.add(chatWidget, 'jupyter-ai-chat');
     }
+
+    /**
+     * Set the theme manager
+     */
+    setThemeManager(manager);
   }
 };
 
